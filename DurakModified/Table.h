889ceb807a27c2b023eee1585909d6e637c74b53
kvitka-dykogo-granatu <99298961+kvitka-dykogo-granatu@users.noreply.github.com>
2022-09-12 19:53:59 +0300

    Update Table.h

--- DurakModified/Table.h
+++ DurakModified/Table.h
@@ -44,27 +44,6 @@ struct Table
 			cout << "	";
 		}
 		cout << endl;
-		/* //CARDS ON THE BATTLE TABLE
-			CONSOLE_SCREEN_BUFFER_INFO csbuf_table;
-		HANDLE hStdOut_table = GetStdHandle(STD_OUTPUT_HANDLE);
-		GetConsoleScreenBufferInfo(hStdOut_table, &csbuf_table);
-		char assist_l[6] = { '.', '-', '.', '|', '\'', '\'' };
-		COORD pos = { csbuf_table.dwMaximumWindowSize.X / 2 - 40, 12 };
-		cout << "			";
-		pos.X += 19;
-		SetConsoleCursorPosition(hStdOut_table, pos);
-		for (int i = 0; i < Battle.size(); i++)
-		{
-			pos.X += 6 + 2;
-			Battle[i][0].print();
-			draw_border(4, 3, { static_cast<SHORT>(pos.X - 1), static_cast<SHORT>(pos.Y - 2) }, assist_l, csbuf_table, hStdOut_table);
-			cout << "  /";
-			if(Battle[i][1].getPower()!=0)
-				Battle[i][1].print();
-			cout << "  ";
-		}
-		cout << endl;
-		*/
 	}
 };
 
